6639e5b43ecd3db193353d8273083108fcc784fa
Egor Nazarov <nazarov73595@yandex.ru>
2022-06-14 14:06:26 +0400

    Выполнено 6 домашние задание

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat extends Animals{
+    public Cat(String nickname) { super(nickname, "кошка", 200,0);
+
+    }
+
+    @Override
+    public void doAction() {
+        System.out.println(getTypeOfAnimal() + " " + getNickname() + " " + "пробежала 200м и не умеет плавать");
+    }
+
 }
--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Dog {
+public class Dog extends Animals {
+    public Dog(String nickname) { super(nickname, "собака", 500, 10);
+    }
+
+    @Override
+    public void doAction() {
+        System.out.println( getTypeOfAnimal() + " " + getNickname() + " " + "пробежал 500м и проплыл 10м");
+    }
+
 }
